199672a7141734f14287cb0ec9fa6533fa9fa1c7
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2024-10-20 11:13:48 +0100

    Add tests with multiple random seeds

--- app/features/match-generation/match-pairs.test.ts
+++ app/features/match-generation/match-pairs.test.ts
@@ -3,11 +3,12 @@ import { createGraph } from './create-graph';
 import { MatchPairsResult, matchPairs } from './match-pairs';
 
 const mockMath = Object.create(global.Math);
-mockMath.random = () => 0.5;
+mockMath.random = vi.fn();
 const originalMath = global.Math;
 
 describe('match-pairs', () => {
     beforeEach(() => {
+        mockMath.random.mockReturnValue(0.5);
         global.Math = mockMath;
     });
 
@@ -20,81 +21,72 @@ describe('match-pairs', () => {
         expect(Math.random()).toBe(0.5);
     });
 
-    it('should make a simple loop with 2 members', () => {
-        const members = [
-            {
-                id: '0',
-                name: 'Alice',
-                exclusions: [],
+    describe.each([
+        { seed: 0 },
+        { seed: 0.1 },
+        { seed: 0.2 },
+        { seed: 0.3 },
+        { seed: 0.4 },
+        { seed: 0.5 },
+        { seed: 0.6 },
+        { seed: 0.7 },
+        { seed: 0.8 },
+        { seed: 0.9 },
+    ])('with random seed $seed', ({ seed }) => {
+        it.each([[2], [3], [4], [5], [6], [7], [8], [9], [10]])(
+            'should return unsolved if loop (%i members) is not possible because of exclusions',
+            (numMembers) => {
+                mockMath.random.mockReturnValue(seed);
+                const members = makeInvalidMembers(numMembers);
+
+                const graph = createGraph(members);
+
+                const result = matchPairs(graph);
+
+                expect(result).toBe('unsolved');
             },
-            {
-                id: '1',
-                name: 'Bob',
-                exclusions: [],
-            },
-        ];
+        );
 
-        const graph = createGraph(members);
+        it.each([
+            [2],
+            [3],
+            [4],
+            [5],
+            [6],
+            [7],
+            [8],
+            [9],
+            [10],
+            [11],
+            [12],
+            [13],
+            [14],
+            [15],
+        ])('should make a loop with %i members', (numMembers) => {
+            mockMath.random.mockReturnValue(seed);
+            const members = makeMembers(numMembers);
 
-        const pairings = matchPairs(graph);
+            const graph = createGraph(members);
 
-        expect(pairings).toHaveLength(2);
-        expect(pairings).toContainEqual({
-            giver: members[0],
-            receiver: members[1],
+            const result = matchPairs(graph);
+
+            assertValidSolution(members, result);
         });
-        expect(pairings).toContainEqual({
-            giver: members[1],
-            receiver: members[0],
-        });
     });
 
-    it('should return unsolved if loop is not possible because of exclusions', () => {
-        const members = [
-            {
-                id: '0',
-                name: 'Alice',
-                exclusions: ['1'],
-            },
-            {
-                id: '1',
-                name: 'Bob',
-                exclusions: [],
-            },
-        ];
+    const makeMembers = (count: number) =>
+        Array.from({ length: count }, (_, i) => ({
+            id: i.toString(),
+            name: `Member ${i}`,
+            exclusions: [],
+        }));
 
-        const graph = createGraph(members);
-
-        const result = matchPairs(graph);
-
-        expect(result).toBe('unsolved');
-    });
-
-    it('should make a loop with 3 members', () => {
-        const members = [
-            {
-                id: '0',
-                name: 'Alice',
-                exclusions: [],
-            },
-            {
-                id: '1',
-                name: 'Bob',
-                exclusions: [],
-            },
-            {
-                id: '2',
-                name: 'Charlie',
-                exclusions: [],
-            },
-        ];
-
-        const graph = createGraph(members);
-
-        const result = matchPairs(graph);
-
-        assertValidSolution(members, result);
-    });
+    const makeInvalidMembers = (count: number) =>
+        Array.from({ length: count }, (_, i) => ({
+            id: i.toString(),
+            name: `Member ${i}`,
+            exclusions: Array.from({ length: count }, (_, j) => j.toString()), // Exclude all members
+        }));
 
     const assertValidSolution = (
         members: Array<Member>,
